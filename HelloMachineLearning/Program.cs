b70996f8dc874d1a64fbd56a1bb3acc71b1b68be
Kim-Jong-Jonas <69483396+Kim-Jong-Jonas@users.noreply.github.com>
2021-02-16 21:59:56 +0100

    ML is too damn hard so I'll just play around a bit
    This program just checks how likely one letter is to come after another using a 2d int array the length of the Swedish alphabet. It then randomizes letters from the Swedish alphabet and checks if the combination is commonly used in names in Sweden. The name list comes from SCB.
    Hopefully I'll learn some ML to make the program better in the future. Pretty darn useful program, right?

---
+++ HelloMachineLearning/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.ML;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HelloMachineLearning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lengthOfName = 5; //Enter desired length of your new name
+            int accuracy = 60; //Enter desired accuract of letter selection
+
+            string alphabet = "abcdefghijklmnopqrstuvwxyzåäö";
+            int[,] combinationScore = new int[alphabet.Length, alphabet.Length];
+            string path = AppDomain.CurrentDomain.BaseDirectory + "/SwedishFirstNames";
+            var random = new Random();
+            string newName = "";
+
+
+            string[] names = File.ReadAllLines(path);
+
+            
+
+            foreach (string name in names)
+            {
+                for (int i = 1; i < name.Length; i++)
+                {
+                    if (alphabet.Contains(name[i - 1]) && alphabet.Contains(name[i]))
+                        combinationScore[alphabet.IndexOf(name[i - 1]), alphabet.IndexOf(name[i])]++;
+                }
+            }
+
+            newName += alphabet[random.Next(0, alphabet.Length)];
+
+            for (int i = 0; i < lengthOfName; i++)
+            {
+
+                char temp = alphabet[random.Next(0, alphabet.Length)];
+
+                if (combinationScore[alphabet.IndexOf(newName[i]), alphabet.IndexOf(temp)] > accuracy)
+                    newName += temp;
+
+                else
+                    i--;
+            }
+
+
+
+            Console.WriteLine(newName);
+
+            Console.ReadLine();
+        }
+    }
+}
